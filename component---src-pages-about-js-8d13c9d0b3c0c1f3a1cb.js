57046591db331ef47c1fad7a7829d80e4196c68d
Amresh Kumar <amresh.kumar@idslogic.com>
2019-11-15 18:35:46 +0530

    Updates

---
+++ component---src-pages-about-js-8d13c9d0b3c0c1f3a1cb.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"3XHS":function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),o=a("Wbzz"),c=a("Bl7J"),p=a("vrFN");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(p.a,{title:"About Us"}),l.a.createElement("h1",null,"Hi from the About page"),l.a.createElement("p",null,"Welcome to page About"),l.a.createElement(o.Link,{to:"/sample-page"},"sample-page"),l.a.createElement(o.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-8d13c9d0b3c0c1f3a1cb.js.map
